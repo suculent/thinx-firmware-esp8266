1a0e13c1efd3fb7bf4e6d30d3abe4757df2dadcb
Matěj Sychra <matej.sychra@t-mobile.cz>
2017-06-09 17:29:59 +0200

    aligned Arduino C with API v1.6

--- thinx-firmware-esp8266-arduinoc/Thinx.h
+++ thinx-firmware-esp8266-arduinoc/Thinx.h
@@ -5,15 +5,14 @@
 // build-time constants
 static const String thinx_commit_id = "869fae3fc7d3a1f148e02bfa3da482f4d0ccfc4a";
 static const String thinx_mqtt_url = "thinx.cloud"; // should try thinx.local first for proxy
+static const String thinx_cloud_url = "thinx.cloud"; // will change to proxy!
 static const String thinx_firmware_version_short = "1.5.63";
 static const String thinx_firmware_version = "thinx-firmware-esp8266-1.5.63:2017-05-20"; // TODO: inject thinx_firmware_version_short here
 
 // dynamic variables
-String thinx_cloud_url = "thinx.cloud"; // will change to proxy!
 String thinx_alias = "esp8266-firmware-dev";
-String thinx_api_key = "310c20ed9dcd4663551bd04cc53a58a52c8d9f1b";
-
-String thinx_owner = "test";
+String thinx_api_key = "88eb20839c1d8bf43819818b75a25cef3244c28e77817386b7b73b043193cef4";
+String thinx_owner = "eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f";
 String thinx_udid =  "aa269410-3e45-11e7-ae13-015ac23866dd";
 
 int thinx_mqtt_port = 1883;
