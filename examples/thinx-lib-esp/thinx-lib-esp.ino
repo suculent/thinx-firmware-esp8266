f95c55fca3727c47c9eac5d8249fdbc6f67dacfe
Matěj Sychra <suculent@me.com>
2017-10-24 17:09:11 +0200

    initialization fix (via @hosek)

--- examples/thinx-lib-esp/thinx-lib-esp.ino
+++ examples/thinx-lib-esp/thinx-lib-esp.ino
@@ -12,7 +12,7 @@
 THiNX thx;
 
 // 3. Enter your API Key
-String apikey = "71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732";
+const char* apikey = "71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732";
 
 void setup() {
   Serial.begin(115200);
