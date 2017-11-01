98f008b7ecacce9fb362ac45042f46542aeab4d8
Matěj Sychra <suculent@me.com>
2017-11-01 23:24:58 +0100

    version sync

--- src/thinx.h
+++ src/thinx.h
@@ -4,10 +4,10 @@
 
 const char * THINX_ALIAS = "unnamed";
 const char * THINX_API_KEY = "0";
-const char * THINX_APP_VERSION = "thinx-arduinoc-2.1.112:2017-11-01";
+const char * THINX_APP_VERSION = "thinx-arduinoc-2.1.117:2017-11-01";
 const char * THINX_CLOUD_URL = "thinx.cloud";
 const char * THINX_COMMIT_ID = "771478cc401ce3285c066e20b52993d7e3979075";
-const char * THINX_FIRMWARE_VERSION = "thinx-arduinoc-2.1.112:2017-11-01";
+const char * THINX_FIRMWARE_VERSION = "thinx-arduinoc-2.1.117:2017-11-01";
 const char * THINX_FIRMWARE_VERSION_SHORT = "90";
 const char * THINX_MQTT_URL = "thinx.cloud";
 const char * THINX_OWNER = "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12";
