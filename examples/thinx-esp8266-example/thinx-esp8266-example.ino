ef3b09118d0c852cefeb89c940337eefa44acaee
Matěj Sychra <suculent@me.com>
2017-11-05 22:03:58 +0100

    updated example

--- examples/thinx-esp8266-example/thinx-esp8266-example.ino
+++ examples/thinx-esp8266-example/thinx-esp8266-example.ino
@@ -25,7 +25,41 @@ THiNX thx;
 /* Called after library gets connected and registered */
 void finalizeCallback () {
   Serial.println("*INO: Finalize callback called.");
-  ESP.deepSleep(3e9);
+  //ESP.deepSleep(3e9);
+}
+
+/* Example of using Environment variables */
+void pushConfigCallback (String config) {
+
+  // Convert incoming JSON string to Object
+  DynamicJsonBuffer jsonBuffer(512);
+  JsonObject& root = jsonBuffer.parseObject(config.c_str());
+  JsonObject& configuration = root["configuration"];
+
+  if ( !configuration.success() ) {
+    Serial.println(F("Failed parsing configuration."));
+  } else {
+
+    // Parse and apply your Environment vars
+    const char *ssid = configuration["THINX_ENV_SSID"];
+    const char *pass = configuration["THINX_ENV_PASS"];
+
+    // password may be empty string
+    if ((strlen(ssid) > 2) && (strlen(pass) > 0)) {
+      WiFi.disconnect();
+      WiFi.begin(ssid, pass);
+      long timeout = millis() + 20000;
+      Serial.println("Attempting WiFi migration...");
+      while (WiFi.status() != WL_CONNECTED) {
+        if (millis() > timeout) break;
+      }
+      if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("WiFi migration failed."); // TODO: Notify using publish() to device status channel
+      } else {
+        Serial.println("WiFi migration successful."); // TODO: Notify using publish() to device status channel
+      }
+    }
+  }
 }
 
 void setup() {
@@ -38,10 +72,9 @@ void setup() {
 #endif
 
   thx = THiNX(apikey);
-
-  // Enter API Key and Owner ID (should be faster)
-  //thx = THiNX(apikey, owner_id);
+  thx.setStatus("Hello internet!");
   thx.setFinalizeCallback(finalizeCallback);
+  thx.setPushConfigCallback(pushConfigCallback);
 }
 
 /* Loop must call the thx.loop() in order to pickup MQTT messages and advance the state machine. */
