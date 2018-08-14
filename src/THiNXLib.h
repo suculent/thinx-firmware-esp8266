e60ad0f98d70112ff03ed00c85313aa724f10b55
Matěj Sychra <suculent@me.com>
2018-08-14 12:53:07 +0200

    version bump

--- src/THiNXLib.h
+++ src/THiNXLib.h
@@ -9,14 +9,14 @@
 
 // Provides placeholder for THINX_FIRMWARE_VERSION_SHORT
 #ifndef VERSION
-#define VERSION "2.3.185"
+#define VERSION "2.3.186"
 #endif
 
 #ifndef THX_REVISION
 #ifdef THINX_FIRMWARE_VERSION_SHORT
 #define THX_REVISION THINX_FIRMWARE_VERSION_SHORT
 #else
-#define THX_REVISION "185"
+#define THX_REVISION "186"
 #endif
 #endif
 
