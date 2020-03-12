39b7c37aa556a61ca784dfd048f339f22b19753f
Matej Sychra <suculent@me.com>
2020-03-12 22:24:15 +0100

    synced json and properties library name

--- src/thinx.h
+++ src/thinx.h
@@ -4,11 +4,11 @@
 
 const char * THINX_ALIAS = "unnamed";
 const char * THINX_API_KEY = "";
-const char * THINX_APP_VERSION = "thinx-lib-esp8266-arduino:2.8.243:2020-01-11";
+const char * THINX_APP_VERSION = "thinx-lib-esp8266-arduino:2.8.246:2020-03-12";
 const char * THINX_CLOUD_URL = "thinx.cloud";
 const char * THINX_COMMIT_ID = "0";
-const char * THINX_FIRMWARE_VERSION = "thinx-lib-esp8266-arduino:2.8.243";
-const char * THINX_FIRMWARE_VERSION_SHORT = "2.8.243";
+const char * THINX_FIRMWARE_VERSION = "thinx-lib-esp8266-arduino:2.8.246";
+const char * THINX_FIRMWARE_VERSION_SHORT = "2.8.246";
 const char * THINX_MQTT_URL = "thinx.cloud";
 const char * THINX_OWNER = "0";
 const char * THINX_PLATFORM  = "platformio";
